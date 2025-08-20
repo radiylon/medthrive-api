c62238efefc873fcdbe3fdf3cb9ffd40d98bb5bb
radiylon <radiylon@gmail.com>
2025-08-19 23:34:29 -0700

    Update README and remove dev resources

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ MEDTHRIVE is an example medication management app for caregivers.  I built this 
 
 Built using TypeScript, AWS API Gateway, AWS Lambda, and Amazon RDS via SST.
 
-[App Demo](https://d3aozqk9inqzy1.cloudfront.net/)
+[App Demo](https://d155y5xplx1o61.cloudfront.net/)
 
 [Project Notes](https://docs.google.com/document/d/1lZQ15cpvRAOPbPFhcNsnM_V71i-V8CkYG91ndv7g3u0/edit?tab=t.0Z)
 
--- sst-env.d.ts
+++ sst-env.d.ts
@@ -5,26 +5,6 @@
 
 declare module "sst" {
   export interface Resource {
-    "DatabaseMigrator": {
-      "name": string
-      "type": "sst.aws.Function"
-    }
-    "MedApi": {
-      "type": "sst.aws.ApiGatewayV2"
-      "url": string
-    }
-    "MedDatabase": {
-      "database": string
-      "host": string
-      "password": string
-      "port": number
-      "type": "sst.aws.Postgres"
-      "username": string
-    }
-    "MedVpc": {
-      "bastion": string
-      "type": "sst.aws.Vpc"
-    }
   }
 }
 /// <reference path="sst-env.d.ts" />

8901fa1ec27d3ba2deddb5e03dd87252ceb6d49e
radiylon <radiylon@gmail.com>
2025-08-19 23:10:00 -0700

    Update sst config

--- sst.config.ts
+++ sst.config.ts
@@ -4,9 +4,8 @@ export default $config({
   app(input) {
     return {
       name: "medthrive-api",
-      // removal: input?.stage === "production" ? "retain" : "remove",
-      // protect: ["production"].includes(input?.stage),
-      removal: "remove",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
       home: "aws",
       providers: {
         aws: {
